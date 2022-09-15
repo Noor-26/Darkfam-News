ce514990b4f264336d180c8fbf6278dbf61f5a27
Noor-26 <nk26982698@gmail.com>
2022-09-15 20:51:29 +0600

    basic setup done and Navber created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navber from './components/shared/Navber/Navber';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App" data-theme="light">
+      <Navber/>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='capitalize text-3xl text-center'>top news of india</h1>
+    </div>
+  )
+}
+
+export default Home
